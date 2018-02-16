8a1c6e921c0397da5e09f322df1acd8fd40dabe1
Randy Beckford <Randy Beckford@Randy.home>
2018-02-15 22:33:34 -0700

    added the Test controls

---
+++ OreganTrail/test/byui/cit260/oreganTrail/control/HuntControlTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oreganTrail.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Randy Beckford
+ */
+public class HuntControlTest {
+    
+    public HuntControlTest() {
+    }
+
+    /**
+     * Test of calcHuntScene method, of class HuntControl.
+     */
+    @Test
+    public void testCalcHuntScene() {
+        System.out.println("calcHuntScene");
+        String animal = "";
+        StringtextBang = null;
+        double expResult = 0.0;
+        double result = HuntControl.calcHuntScene(animal, <error>);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
