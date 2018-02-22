88e6520276b8bbf5a288a4e32857d038e86b2348
Randy ther Master <Randy ther Master@LAPTOP-K46JI4VR.home>
2018-02-21 22:14:45 -0700

    made some addition to the game for the view layer.

---
+++ OreganTrail/src/byui/cit260/oreganTrail/view/StartProgramView.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oreganTrail.view;
+
+/**
+ *
+ * @author Randy ther Master
+ */
+public class StartProgramView {
+
+    public StartProgramView() {
+      
+   
+    displayStartProgramView() {
+    endOfView = false
+    DO
+    inputs = getInputs()
+    IF no inputs were entered OR the first input is Q
+    RETURN
+    ENDIF
+    endOfView = doAction(inputs)
+    WHILE endOfView != true
+}
+    }
+
+    private void displayStartProgramView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
